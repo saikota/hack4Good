ef9a54d82a2a1341eb87fe3092fbf6b303ac57d2
Sai Kota Mac <sai.kota@hotmail.com>
2015-04-16 05:04:40 -0700

    updated mongourl

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,7 +5,7 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/hak4good-dev'
+    uri: 'mongodb://<dbuser>:<dbpassword>@ds029640.mongolab.com:29640/hak4good'
   },
 
   seedDB: true
