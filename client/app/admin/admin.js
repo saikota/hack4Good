f8bd1bcd887b4cd509741ced2aac84aacaa1311b
Sai Kota Mac <sai.kota@hotmail.com>
2015-04-16 01:59:53 -0700

    Initial Commit 1.0. Working Copy of Angular Mongo Express and Node using Yeoman Angular Full Stack

---
+++ client/app/admin/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('hak4GoodApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/admin', {
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
